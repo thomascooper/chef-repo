143b5c1e825cd69b4eaa305b0a87efb0f04ff44a
root <root@debian>
2015-04-28 15:35:21 -0400

    added timezone and longview

--- cookbooks/base-linode/attributes/default.rb
+++ cookbooks/base-linode/attributes/default.rb
@@ -1,2 +1,3 @@
-default["set_fqdn"] = node["base-linode"]["hostname"]
+default["set_fqdn"] = node["base_linode"]["hostname"]
 default["hostname_cookbook"]["use_node_ip"] = true
+default["timezone"]["zone"] = "America/New_York"
--- cookbooks/base-linode/recipes/default.rb
+++ cookbooks/base-linode/recipes/default.rb
@@ -9,4 +9,11 @@
 
 include_recipe "git"
 include_recipe "hostnames"
+include_recipe "timezone"
 
+execute "Install Longview" do
+  if node[:base_linode].attribute?("longview_id")
+    id = node["base_linode"]["longview_id"]
+    command "curl -s https://lv.linode.com/#{id} | sudo bash"
+  end
+end
