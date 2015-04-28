8249d5dac3eea9936ad2857d4498bb81ae058337
root <root@debian>
2015-04-28 06:06:55 +0000

    added hostname

--- cookbooks/base-linode/recipes/default.rb
+++ cookbooks/base-linode/recipes/default.rb
@@ -7,5 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "fixhost" do
+  command "echo 'wwi_node' > /etc/hostname"
+  command "hostname -F /etc/hostname"
+end
+
 include_recipe "git"
+include_recipe "hostnames"
 
